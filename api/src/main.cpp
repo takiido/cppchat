d9b7fdcd4ac954d01c7e6e78e85692615e4b371b
Alex Tovshchyk <157808651+takiido@users.noreply.github.com>
2025-05-07 16:16:29 -0500

    Initial project structure and CMake setup for api, app, and server.

---
+++ api/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, cppchat_api!" << std::endl;
+    return 0;
+}
---
+++ app/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, cppchat_app!" << std::endl;
+    return 0;
+}
