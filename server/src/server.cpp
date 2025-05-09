f657328fa24af4d16c9814cb356baba3073024c5
Alex Tovshchyk <157808651+takiido@users.noreply.github.com>
2025-05-08 20:43:12 -0500

    This can wait for a while

--- server/src/server.cpp
+++ server/src/server.cpp
@@ -44,7 +44,7 @@ namespace cppchat::server {
         if (const auto search = clients_by_username.find(msg.receiver.value_or(""));
             search != clients_by_username.end()) {
             std::cout << "Found " << search->first << ' ' << search->second << '\n';
-            search->second->send_message(msg);
+            // search->second->send_message(msg);
         } else std::cout << "Not found\n";
 
         std::cout << "Got something here for " << msg.receiver.value_or("empty")
