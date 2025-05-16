748293a83c9a20c9e7a5db9ba2561f210055a1c6
Alex Tovshchyk <157808651+takiido@users.noreply.github.com>
2025-05-16 16:21:03 -0500

    Add libclang as submodule

---
+++ api/include/reflect.h
@@ -0,0 +1,8 @@
+//
+// Created by Alex Tovshchyk on 2025-05-15.
+//
+
+#ifndef REFLECT_H
+#define REFLECT_H
+
+#endif //REFLECT_H
---
+++ tools/migrate/main.cpp
@@ -0,0 +1,3 @@
+//
+// Created by Alex Tovshchyk on 2025-05-15.
+//
